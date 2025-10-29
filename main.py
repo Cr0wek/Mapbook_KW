1f0337eaec81422ec7b0879c471fd8c76af5e49d
krzysztofwroniak <cr0wss@outlook.com>
2025-10-29 09:41:30 +0100

    wip: list of names, locations, posts

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-uzytkownicy:list=[]
-uzytkownicy.append('Krzysztof')
-uzytkownicy.append('Kasia')
-uzytkownicy.append('Antek')
-uzytkownicy.append('Bartek')
 
-for uzytkownik in uzytkownicy:
-    print(f'Hello {uzytkownik}')
+users:list=[
+    {'name': 'Krzysztof', 'location': 'Wolomin', 'posts': 1},
+    {'name': 'Bartek', 'location': 'Warszawa', 'posts': 4},
+    {'name': 'Antek', 'location': 'Berlin', 'posts': 6},
+    {'name': 'Kasia', 'location': 'Olsztyn', 'posts': 8},
+    
+]
+
+for user in users:
+    print(f'Twoj znajomy {user['name']} opublikowal z miejscowosci {user['location']} tyle postów: {user['posts']}')
 
 
