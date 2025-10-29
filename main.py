64036e4ad7b4aa181480bbc7759e83f319f03bc9
krzysztofwroniak <cr0wss@outlook.com>
2025-10-29 11:01:14 +0100

    wip: added function add new user

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ users:list=[
     {'name': 'Bartek', 'location': 'Warszawa', 'posts': 4},
     {'name': 'Antek', 'location': 'Berlin', 'posts': 6},
     {'name': 'Kasia', 'location': 'Olsztyn', 'posts': 8},
-    
+
 ]
 def user_info(usersdata:list)->None:
     for user in usersdata:
         print(f'Twoj znajomy {user['name']} opublikowal z miejscowosci {user['location']} tyle postów: {user['posts']}')
+def addUser(usersdata)->None:
+    name:str=input("Podaj imie nowego znajomego: ")
+    location:str=input("Podaj nazwe miejscowosci: ")
+    posts:int=input("Podaj liczbe postow: ")
+    usersdata.append({"name":name, "location":location, "posts":posts})
+
 while True:
     tmp_choice:int=int(input('Wybierz opcje menu: '))
     if tmp_choice==0:
@@ -18,6 +24,7 @@ while True:
         user_info(users)
     if tmp_choice==2:
         print('Wybrano funkcje dodawania znajomego')
+        addUser(users)
     if tmp_choice==3:
         print('Wybrano funkcje usuwania znajomego')
     if tmp_choice==4:
