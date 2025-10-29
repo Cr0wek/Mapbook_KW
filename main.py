db8c7e2ee7d5945bc4ab49b9e7744999fa91ab06
krzysztofwroniak <cr0wss@outlook.com>
2025-10-29 10:37:00 +0100

    wip: added function display user info

--- main.py
+++ main.py
@@ -6,8 +6,21 @@ users:list=[
     {'name': 'Kasia', 'location': 'Olsztyn', 'posts': 8},
     
 ]
+def user_info(usersdata:list)->None:
+    for user in usersdata:
+        print(f'Twoj znajomy {user['name']} opublikowal z miejscowosci {user['location']} tyle postów: {user['posts']}')
+while True:
+    tmp_choice:int=int(input('Wybierz opcje menu: '))
+    if tmp_choice==0:
+        break
+    if tmp_choice==1:
+        print('Wybrano funkcje wyswietlania aktywnosci znajomych')
+        user_info(users)
+    if tmp_choice==2:
+        print('Wybrano funkcje dodawania znajomego')
+    if tmp_choice==3:
+        print('Wybrano funkcje usuwania znajomego')
+    if tmp_choice==4:
+        print('Wybrano funkcje aktyalizacji znajomego')
 
-for user in users:
-    print(f'Twoj znajomy {user['name']} opublikowal z miejscowosci {user['location']} tyle postów: {user['posts']}')
-
-
+    
